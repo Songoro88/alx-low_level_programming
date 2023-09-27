931e5856cd6678c49ac16fac924a6c7242b430b9
Songoro88 <songoro88@gmail.com>
2023-09-27 15:45:56 +0300

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * Sum_them_all - returns the sum of all paramters.
+ * @n: The number of paramters passed to the Function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If x == 0 - 0.
+ *         Otherwise - the sum of all parameters.
+ */
+int sum_them_all(const unsigned int x, ...)
+{
+	va_list ap;
+	unsigned int j, sum = 0;
+
+	va_start(ap, x);
+
+	for (j = 0; j < x; j++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * Print_numbers - Prints Numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
+ */
+void print_numbers(const char *separator, const unsigned int x, ...)
+{
+	va_list nums;
+	unsigned int index;
+
+	va_start(nums, x);
+
+	for (index = 0; index < x; index++)
+	{
+		printf("%d", va_arg(nums, int));
+
+		if (index != (x - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\x");
+
+	va_end(nums);
+}
